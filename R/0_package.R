0d07d39f6216ecae16590996c21f14b74bf403dc
Robrecht Cannoodt <brechtus@gmail.com>
2018-02-21 10:43:15 +0100

    initial commit

---
+++ R/0_package.R
@@ -0,0 +1,9 @@
+#' Organising my workspace
+#'
+#' This package contains code to organise code and data within my workspace.
+#'
+#' @docType package
+#' @name workspace
+#'
+NULL
+
---
+++ R/setup_experiment.R
@@ -0,0 +1,84 @@
+#' Find the workspace path
+#'
+#' @export
+get_workspace_folder <- function() {
+  pwd <- Sys.getenv("MY_WORKSPACE_PATH")
+
+  if (pwd == "") {
+    stop("Workspace path could not be found. Did you export a MY_WORKSPACE_PATH variable?")
+  }
+
+  pwd
+}
+
+
+#' Helper function for controlling projects
+#'
+#' @param project_id id for the project
+#' @param filename the filename
+#'
+#' @export
+#'
+#' @rdname project
+#'
+#' @examples
+#' \dontrun{
+#' project("test_plots")
+#'
+#' data <- matrix(runif(200), ncol = 2)
+#' pdf(figure_file("testplot.pdf"), 5, 5)
+#' plot(data)
+#' dev.off()
+#' }
+project <- function(project_id) {
+  # check whether the working directory is indeed the dynalysis folder
+  dyn_fold <- get_dynalysis_folder()
+
+  # set option
+  options(workspace_project_id = project_id)
+}
+
+# create a helper function
+project_subfolder <- function(path) {
+  function(filename = "", project_id = NULL) {
+    filename <- paste0(filename, collapse = "")
+
+    dyn_fold <- get_workspace_folder()
+
+    # check whether exp_id is given
+    if (is.null(project_id)) {
+      project_id <- getOption("workspace_project_id")
+    }
+
+    # check whether exp_fold could be found
+    if (is.null(project_id)) {
+      stop("No project folder found. Did you run project(...) yet?")
+    }
+
+    # determine the full path
+    full_path <- paste0(dyn_fold, "/", path, "/", project_id, "/")
+
+    # create if necessary
+    dir.create(full_path, recursive = TRUE, showWarnings = FALSE)
+
+    # get complete filename
+    paste(full_path, filename, sep = "")
+  }
+}
+
+#' @rdname project
+#' @export
+code_file <- project_subfolder("projects")
+
+#' @rdname project
+#' @export
+derived_file <- project_subfolder("data/derived_data")
+
+#' @rdname project
+#' @export
+raw_file <- project_subfolder("data/raw_data")
+
+#' @rdname project
+#' @export
+result_file <- project_subfolder("data/figures")
+
